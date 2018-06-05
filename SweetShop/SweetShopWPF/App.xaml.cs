bdf853adeb9efdbbf5a6051cdea9b31fffbacc74
AbakumovaOlga <abakumova-olga-a@ya.ru>
2018-06-05 16:59:03 +0400

    laba11

--- SweetShop/SweetShopWPF/App.xaml.cs
+++ SweetShop/SweetShopWPF/App.xaml.cs
@@ -1,9 +1,12 @@
-﻿using SweetShopService.ImplementationsList;
+﻿using SweetShopService;
+using SweetShopService.ImplementationsBD;
+using SweetShopService.ImplementationsList;
 using SweetShopService.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -32,12 +35,13 @@ namespace SweetShopWPF
         public static IUnityContainer BuildUnityContainer()
         {
             var currentContainer = new UnityContainer();
-            currentContainer.RegisterType<ICustomerService, CustomerServiceList>(new HierarchicalLifetimeManager());
-            currentContainer.RegisterType<IIngredientService, IngredientServiceList>(new HierarchicalLifetimeManager());
-            currentContainer.RegisterType<IBakerService, BakerServiceList>(new HierarchicalLifetimeManager());
-            currentContainer.RegisterType<ICakeService, CakeServiceList>(new HierarchicalLifetimeManager());
-            currentContainer.RegisterType<IFridgeService, FridgeServiceList>(new HierarchicalLifetimeManager());
-            currentContainer.RegisterType<IMainService, MainServiceList>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<DbContext, AbstractDbContext>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<ICustomerService, CustomerServiceBD>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<IIngredientService, IngredientServiceBD>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<IBakerService, BakerServiceBD>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<ICakeService, CakeServiceBD>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<IFridgeService, FridgeServiceBD>(new HierarchicalLifetimeManager());
+            currentContainer.RegisterType<IMainService, MainServiceBD>(new HierarchicalLifetimeManager());
 
             return currentContainer;
         }
